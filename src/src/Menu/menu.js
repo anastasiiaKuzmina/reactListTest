7e74ed3b23d956c4476836e95057689378a2e29e
Anastasiia_Taletskai <anastasiia_taletskaia@epam.com>
2017-12-19 18:14:51 +0300

    update menu component

---
+++ src/src/Menu/menu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../menu.css';
+
+class Menu extends Component {
+    render() {
+        return (
+            <div>
+                <h2>menu</h2>
+            </div>
+        );
+    }
+}
+
+export default Menu;
